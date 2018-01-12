f29502ad3a09a72b33c3cd592c8e56d075784ff7
GoDotDotDot <chukui0202@gmail.com>
2018-01-12 09:27:51 +0800

    update

--- app/app.layout.js
+++ app/app.layout.js
@@ -14,18 +14,7 @@ import history from './common/history'
 import './common/global.scss'
 
 const Option = Select.Option;
-const menu = (
-  <Menu>
-    <Menu.Item key="0">
-      <a href="http://www.alipay.com/">1st menu item</a>
-    </Menu.Item>
-    <Menu.Item key="1">
-      <a href="http://www.taobao.com/">2nd menu item</a>
-    </Menu.Item>
-    <Menu.Divider />
-    <Menu.Item key="3">3rd menu item</Menu.Item>
-  </Menu>
-);
+
 // import Promise from "es6-promise";
 // Promise.polyfill();
 
