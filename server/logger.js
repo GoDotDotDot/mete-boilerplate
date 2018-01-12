a71824b577b67c5d01931b7c24e1c39d3c13c04f
GoDotDotDot <chukui0202@gmail.com>
2018-01-12 10:25:13 +0800

    specail version

--- server/logger.js
+++ server/logger.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-console */
 
-const chalk = require('chalk');
-const ip = require('ip');
+const chalk = require('chalk')
+const ip = require('ip')
 
-const divider = chalk.gray('\n-----------------------------------');
+const divider = chalk.gray('\n-----------------------------------')
 
 /**
  * Logger middleware, you can customize it to make messages more personal
@@ -12,26 +12,28 @@ const logger = {
 
   // Called whenever there's an error on the server we want to print
   error: (err) => {
-    console.error(chalk.red(err));
+    console.error(chalk.red(err))
   },
 
   // Called when express.js app starts on given port w/o errors
   appStarted: (port, host, tunnelStarted) => {
-    console.log(`Server started ! ${chalk.green('✓')}`);
-
+    // chalk.yellowBright(' Best wishes!')
+    console.log(chalk.bgBlackBright(`${chalk.green('This is a special version for little fairy!')} ${chalk.yellowBright(' Best wishes!')}`))
+    console.log(`More be found: ${chalk.blue(`${chalk.italic('https://github.com/GoDotDotDot/mete-boilerplate/tree/special')}`)}`)
+    console.log(`Server started ! ${chalk.green('✓')}`)
     // If the tunnel started, log that and the URL it's available at
     if (tunnelStarted) {
-      console.log(`Tunnel initialised ${chalk.green('✓')}`);
+      console.log(`Tunnel initialised ${chalk.green('✓')}`)
     }
 
     console.log(`
-${chalk.bold('Access URLs:')}${divider}
-Localhost: ${chalk.magenta(`http://${host}:${port}`)}
-      LAN: ${chalk.magenta(`http://${ip.address()}:${port}`) +
-(tunnelStarted ? `\n    Proxy: ${chalk.magenta(tunnelStarted)}` : '')}${divider}
-${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}
-    `);
-  },
-};
+      ${chalk.bold('Access URLs:')}${divider}
+      Localhost: ${chalk.magenta(`http://${host}:${port}`)}
+            LAN: ${chalk.magenta(`http://${ip.address()}:${port}`) +
+      (tunnelStarted ? `\n    Proxy: ${chalk.magenta(tunnelStarted)}` : '')}${divider}
+      ${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}
+          `)
+  }
+}
 
-module.exports = logger;
+module.exports = logger
